d55795e7fed6cc7a640e4a24493ed78ea198d5bb
rbatos <romain.busuttil@atos.net>
2026-05-26 21:57:27 +0200

    chore: Ajout d'un commentaire sur le problème des , - romain

--- services/api-nlp/tests/test_predict.py
+++ services/api-nlp/tests/test_predict.py
@@ -32,12 +32,14 @@ def test_predict_endpoint_valid() -> None:
     assert "sentiment" in body
     assert "scores_5_stars" in body
     assert body["sentiment"] in {"négatif", "neutre", "positif"}
+    logger.info(f"Test texte valide, status code : {response.status_code}, response : {response.json()}")
 
 def test_predict_endpoint_empty_text() -> None:
     """Teste l'endpoint /predict avec un texte vide."""
     with TestClient(app) as client:
         response = client.post("/predict", json={"texte": ""})
     assert response.status_code == 422
+    logger.warning(f"Test texte vide, status code : {response.status_code}, response : {response.json()}")
 
 def test_predict_endpoint_long_text() -> None:
     """Teste l'endpoint /predict avec un texte > 2000 caractères."""
@@ -45,6 +47,7 @@ def test_predict_endpoint_long_text() -> None:
     with TestClient(app) as client:
         response = client.post("/predict", json={"texte": long_text})
     assert response.status_code == 422 
+    logger.warning(f"Test texte long, status code : {response.status_code}, response : {response.json()}")
 
 def test_predict_endpoint_parametres() -> None:
     """Teste l'endpoint /predict avec plusieurs textes du CSV."""
@@ -63,6 +66,11 @@ def test_predict_endpoint_parametres() -> None:
             values = line.strip().split(",")
             review_dict = dict(zip(header, values))  # créer un dictionnaire pour chaque ligne
             reviews.append(review_dict)
+            # A voir... les virgule dans le csv mettent le bazar dans le découpage en colonnes, du coup je me retrouve avec des reviews tronquées
+            # (ex: "Personnel charmant, chambre impeccable, on reviendra !" devient "Personnel charmant" et le reste est perdu). 
+            # Je vais devoir revoir la structure du csv pour éviter ce problème de découpage. 
+            # Peut-être utiliser df = pd.read_csv(csv_file) pour lire le csv et récupérer les reviews sous forme de liste de dictionnaires, ce qui gère mieux les virgules dans les champs.
+            # mais Pandas n'est pas dans les requirements donc pas dans l'environnement de l'API.
             #if len(reviews) >= 3:  # on prend les 3 premières reviews (sans la ligne d'en-tête)
                 #break
     #reviews = csv_file.read_text(encoding="utf-8").splitlines()[1:8]  # on prend les 3 premières reviews (sans la ligne d'en-tête)
